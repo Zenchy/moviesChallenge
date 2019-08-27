5a7bd30b9e19c6b31a48417f3b33696a214ce65d
AlvaroCS <alvaro.corral@innocv.com>
2019-08-27 21:05:04 +0200

    now

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,5 @@
  //eslint no-restricted-globals: 'off' 
- 
+ console.log('now')
 //Iteration 1: All rates average - Get the average of all rates with 2 decimals 
  
 // Iteration 2: Drama movies - Get the average of Drama Movies
